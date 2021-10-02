684d961265ec2916791d0db5cf1e2815635de289
Anubhaw  Sharma <56713895+Anubhaw19@users.noreply.github.com>
2021-10-02 23:18:42 +0530

    Created Strings/ Subsequences.cpp
    Finding out all subsequences of a string. A String is a subsequence of a given String, that is generated by deleting some character of a given string without changing its order.
    
    Examples: 
    
    Input : abc
    Output : a, b, c, ab, bc, ac, abc
    
    Input : aaa
    Output : a, aa, aaa

---
+++ Strings/Subsequences.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Find all subsequences
+void printSubsequence(string input, string output)
+{
+	// Base Case
+	// if the input is empty print the output string
+	if (input.empty()) {
+		cout << output << endl;
+		return;
+	}
+
+	// output is passed with including
+	// the Ist character of
+	// Input string
+	printSubsequence(input.substr(1), output + input[0]);
+
+	// output is passed without
+	// including the Ist character
+	// of Input string
+	printSubsequence(input.substr(1), output);
+}
+
+int main()
+{
+	string output = "";
+	string input;
+  cin >> input;
+
+	printSubsequence(input, output);
+
+	return 0;
+}
