783489735a9093efe447eff8a9ef9679c4530a2f
Vibhu Gautam <vibhuparmar13@gmail.com>
2021-10-21 10:43:39 +0530

    De Brujin Sequence added

---
+++ Graph/De_Brujin.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+unordered_set<string> seen;
+vector<int> edges;
+string a = "01";
+ 
+void dfs(string start)
+{
+    for(int i = 0; i < 2; ++i)
+    {
+        string str = start + a[i];
+        if(seen.find(str) == seen.end())
+        {
+            seen.insert(str);
+            dfs(str.substr(1));
+            edges.push_back(i);
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+ 
+    string start = string(n - 1, '0');
+    dfs(start);
+ 
+    string ans = "";
+ 
+    for(int i = 0; i < (1 << n); ++i)
+        ans += a[edges[i]];
+    ans += start;
+ 
+    cout << ans << '\n';
+}
+
+// Your task is to construct a minimum-length bit string that contains all possible substrings of length n. 
+// For example, when n=2, the string 00110 is a valid solution, because its substrings of length 2 are 00, 01, 10 and 11.
+
+// Input:
+// The only input line has an integer n.
+
+// Output:
+// Print a minimum-length bit string that contains all substrings of length n. You can print any valid solution.
