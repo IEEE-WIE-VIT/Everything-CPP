7b4c7c59de29f1ce27b11395f069f3dead989d21
anonymousdaisy5 <91734640+anonymousdaisy5@users.noreply.github.com>
2021-10-26 14:31:07 +0530

    Traverse using loop
    Traverse using for loop.

---
+++ Arrays/traverse using for loop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+ int arr[7]={10, 0, 20, 0, 30,89, 09}; //creating and initializing array    
+        //traversing array    
+       for (int i: arr)     
+        {    
+            cout<<i<<"\n";    
+        }    
